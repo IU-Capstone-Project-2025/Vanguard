af55cdd06b6f456524ab1d21f7e1fb4bc5e9361f
kolsmer <aminovvolna2005@gmail.com>
2025-06-11 22:32:23 +0300

    added create and join methods

--- backeng-go/internal/shared/Admin.go
+++ backeng-go/internal/shared/Admin.go
@@ -1,7 +1,21 @@
 package shared
 
+import (
+	"fmt"
+	"math/rand"
+)
+
 type Admin struct {
 	Name     string
 	Email    string
 	Password string
+}
+
+func (a Admin) CreateGame() int {
+	minn := 100000
+	maxx := 999999
+	n := rand.Intn(maxx-minn+1) + minn
+	fmt.Println(a.Name, "created game", n)
+	return n
+
 }
--- backeng-go/internal/shared/user.go
+++ backeng-go/internal/shared/user.go
@@ -1,7 +1,16 @@
 package shared
 
+import (
+	"fmt"
+	"strconv"
+)
+
 type User struct {
 	Name     string
 	Email    string
 	Password string
+}
+
+func (u User) JoinGame(code int) {
+	fmt.Println(u.Name, "joined game", strconv.Itoa(code))
 }
