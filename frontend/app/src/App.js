ac788644bd5f7c7e6376326e7294417b65b06edb
Dmitriy Lukiyanov <d.lukiyanov@innopolis.university>
2025-06-19 13:22:55 +0300

    add WaitGameStartAdmin component and update routing; refactor navigation in HomePage and AskToJoinSessionPage

--- frontend/app/src/App.js
+++ frontend/app/src/App.js
@@ -4,6 +4,7 @@ import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import HomePage from './components/HomePage';
 import CreateSessionPage from './components/CreateSessionPage';
 import AskToJoinSessionPage from './components/AskToJoinSessionPage';
+import WaitGameStartAdmin from './components/WaitGameStartAdmin';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Route path='/' element={<HomePage/>}/>
         <Route path='/create' element={<CreateSessionPage/>} />
         <Route path="/ask-to-join/:sessionCode" element={<AskToJoinSessionPage/>} /> 
+        <Route path="/session/:sessionCode" element={<WaitGameStartAdmin/>} />
       </Routes>
     </Router>
   );
