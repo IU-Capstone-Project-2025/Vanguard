49814683559172a6886e9af143f26ae6d3954339
blxxdclxud <ramazanatzuf10@gmail.com>
2025-06-22 02:59:44 +0300

    ws package code was separated into several logical files, the functionality was distributed

---
+++ backend-go/real_time/ws/auth.go
@@ -0,0 +1,38 @@
+package ws
+
+import (
+	"errors"
+	"fmt"
+	"github.com/golang-jwt/jwt/v5"
+	"xxx/real_time/config"
+	"xxx/shared"
+)
+
+func extractTokenData(tokenString string) (*shared.UserTokenClaims, error) {
+	fmt.Println(tokenString)
+	token, err := jwt.ParseWithClaims(tokenString, &shared.UserTokenClaims{}, func(token *jwt.Token) (interface{}, error) {
+		return []byte(config.LoadConfig().JWT.SecretKey), nil
+	})
+
+	if err != nil {
+		return nil, err
+	}
+
+	switch {
+	case token.Valid:
+		fmt.Println("OK token")
+	case errors.Is(err, jwt.ErrTokenMalformed):
+		fmt.Println("Malformed token")
+	default:
+		fmt.Println("Couldn't handle this token:", err)
+	}
+
+	claims, ok := token.Claims.(*shared.UserTokenClaims)
+	if !ok {
+		return nil, fmt.Errorf("error decoding jwt")
+	}
+
+	return claims, nil
+}
+
+// TODO: func validateToken() {}
