9673759f81f2968dd67d559f5127b62d30b83f90
Dmitriy Lukiyanov <d.lukiyanov@innopolis.university>
2025-06-22 00:41:58 +0300

    Fixed some git conflicts manually
    Add EnterNicknamePage component and update routing in App.js

--- frontend/app/src/App.js
+++ frontend/app/src/App.js
@@ -7,6 +7,8 @@ import CreateSessionPage from './components/CreateSessionPage';
 import AskToJoinSessionPage from './components/AskToJoinSessionPage';
 import WaitGameStartAdmin from './components/WaitGameStartAdmin';
 import WaitGameStartPlayer from './components/WaitGameStartPage';
+import JoinGamePage from './components/JoinGamePage';
+import EnterNicknamePage from './components/EnterNicknamePage';
 
 function App() {
   return (
@@ -14,6 +16,8 @@ function App() {
       <Routes>
         <Route path='/' element={<HomePage/>}/>
         <Route path='/create' element={<CreateSessionPage/>} />
+        <Route path='/play' element={<EnterNicknamePage/>} />
+        <Route path='/join' element={<JoinGamePage/>} />
         <Route path="/ask-to-join/:sessionCode" element={<AskToJoinSessionPage/>} /> 
         <Route path="/sessionAdmin/:sessionCode" element={<WaitGameStartAdmin/>} />
         <Route path="/game-session" element={<WaitGameStartPlayer/>} />
