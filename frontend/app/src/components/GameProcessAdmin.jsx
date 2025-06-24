fcfeb8570cba1e3a5e37deebb95fadeef5a407f7
Dmitriy Lukiyanov <d.lukiyanov@innopolis.university>
2025-06-25 00:51:11 +0300

    Enhance GameProcessAdmin: Add answer display functionality and style updates for correct/incorrect options

--- frontend/app/src/components/GameProcessAdmin.jsx
+++ frontend/app/src/components/GameProcessAdmin.jsx
@@ -1,11 +1,20 @@
 import React, { useState, useEffect } from 'react';
 import './styles/GameProcess.css';
+import { useParams } from 'react-router-dom';
 
 const GameProcessAdmin = () => {
   const [quiz, setQuiz] = useState(null);
   const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
   const [answered, setAnswered] = useState(0);
+  const { sessionCode } = useParams();
+  const [serverEndpoint, setServerEndpoint] = useState(sessionStorage.getItem('WSEndpoint') || 'http://localhost:8001');
   const [membersNumber, setMembersNumber] = useState(0);
+  const [isShowCorrect, setIsShowCorrect] = useState(false);
+
+  console.log("Server Endpoint:", serverEndpoint);
+  console.log("Session Code:", sessionCode);
+  console.log("Selected Quiz ID:", sessionStorage.getItem('selectedQuizId'));
+  console.log(quiz);
 
   useEffect(() => {
     // Здесь ты делаешь fetch из бекенда
@@ -17,6 +26,24 @@ const GameProcessAdmin = () => {
     };
     fetchData();
   }, []);
+
+  const showCorrectAnswer = (correctAnswer) => {
+    setIsShowCorrect(true);
+    document.querySelectorAll('.option-button').forEach(button => {
+      if (button.textContent === correctAnswer) {
+        button.classList.add('correct');
+      } else {
+        button.classList.add('incorrect');
+      }
+    });
+  };
+
+  const hideCorrectAnswer = () => {
+    setIsShowCorrect(false);
+    document.querySelectorAll('.option-button').forEach(button => {
+      button.classList.remove('correct', 'incorrect');
+    });
+  };
 
   if (!quiz) {
     return <div className="game-process">Loading Data...</div>;
@@ -41,7 +68,11 @@ const GameProcessAdmin = () => {
       </div>
       <div className="options-grid">
         {currentQuestion.options.map((option, index) => (
-          <button key={index} className="option-button">
+          <button key={index} className={
+              option.is_correct && isShowCorrect
+               ? "option-button correct" : "option-button"
+            }
+          >
             {option.text}
           </button>
         ))}
@@ -57,11 +88,20 @@ const GameProcessAdmin = () => {
             Back
           </button>
         )}
+          <button
+            onClick={() => {
+              showCorrectAnswer(currentQuestion.options.find(option => option.is_correct).text);
+            }}
+            className="nav-button"
+          >
+            Show Answer
+          </button>
         {currentQuestionIndex < quiz.questions.length - 1 && (
           <button
-            onClick={() =>
-              setCurrentQuestionIndex(currentQuestionIndex + 1)
-            }
+            onClick={() => {
+              setCurrentQuestionIndex(currentQuestionIndex + 1);
+              hideCorrectAnswer();
+            }}
             className="nav-button"
           >
             Next
